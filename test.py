03fc8d835440eb6e954df92a82c3eec79f660cf3
elvisvarghese96 <elvisvarghese96@gmail.com>
2023-10-28 20:59:59 -0400

    this is a testing commit

--- test.py
+++ test.py
@@ -1,33 +1,33 @@
-import psycopg2
+# import psycopg2
 
-try:
-    connection = psycopg2.connect(user = 'postgres',
-                                  password="123456",
-                                  host="localhost",
-                                  port="5432",
-                                  database="fastapi")
-    cursor = connection.cursor()
-    postgreSQL_select_Query = "select * from products* "
+# try:
+#     connection = psycopg2.connect(user = 'postgres',
+#                                   password="123456",
+#                                   host="localhost",
+#                                   port="5432",
+#                                   database="fastapi")
+#     cursor = connection.cursor()
+#     postgreSQL_select_Query = "select * from products* "
 
-    cursor.execute(postgreSQL_select_Query)
-    print("Selecting rows from mobile table using cursor.fetchall")
-    mobile_records = cursor.fetchall()
+#     cursor.execute(postgreSQL_select_Query)
+#     print("Selecting rows from mobile table using cursor.fetchall")
+#     mobile_records = cursor.fetchall()
 
-    print("Print each row and it's columns values")
-    for row in mobile_records:
-        print("Id = ", row[0], )
-        print("Model = ", row[1])
-        print("Price  = ", row[2], "\n")
+#     print("Print each row and it's columns values")
+#     for row in mobile_records:
+#         print("Id = ", row[0], )
+#         print("Model = ", row[1])
+#         print("Price  = ", row[2], "\n")
 
-except (Exception, psycopg2.Error) as error:
-    print("Error while fetching data from PostgreSQL", error)
+# except (Exception, psycopg2.Error) as error:
+#     print("Error while fetching data from PostgreSQL", error)
 
-# finally:
-#     # closing database connection.
-#     if connection:
-#         cursor.close()
-#         connection.close()
-#         print("PostgreSQL connection is closed")
+# # finally:
+# #     # closing database connection.
+# #     if connection:
+# #         cursor.close()
+# #         connection.close()
+# #         print("PostgreSQL connection is closed")
 
 
 
